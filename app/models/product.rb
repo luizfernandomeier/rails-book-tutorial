8e5cab7318f255e970bde0d2d98741665473f8c1
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-27 11:16:29 -0300

    feat: improve image regex validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
   validates :title, uniqueness: { case_sensitive: false }
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :image_url, allow_blank: true, format: { with: /[.]((gif)|(jpe?g)|(png))\z/i }
+  # here the `allow_blank` is added so that it doens't return multiple errors, as it is required on the `presence` validation
+  validates :image_url, allow_blank: true, format: { with: /.+\.((gif)|(jpe?g)|(png))\z/i }
 end

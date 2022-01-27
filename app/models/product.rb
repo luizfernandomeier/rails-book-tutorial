89a8cf79e176e3bfd4c89b7d470058f3523b3fcf
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-27 11:19:31 -0300

    feat: improve image validation message

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,9 @@ class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   # here the `allow_blank` is added so that it doens't return multiple errors, as it is required on the `presence` validation
-  validates :image_url, allow_blank: true, format: { with: /.+\.((gif)|(jpe?g)|(png))\z/i }
+  validates :image_url, allow_blank: true, format: {
+    with: /.+\.((gif)|(jpe?g)|(png))\z/i,
+    message: 'must be a URL for GIF, JPG or PNG image.' }
   validate :image_has_to_exist
 
   private

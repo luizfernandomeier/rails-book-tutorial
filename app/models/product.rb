5f3c3449c9bfa06bdcfa7aa7eaa7082e45909696
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-27 11:11:57 -0300

    feat: add product default validators

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :title, uniqueness: { case_sensitive: false }
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :image_url, allow_blank: true, format: { with: /[.]((gif)|(jpe?g)|(png))\z/i }
 end

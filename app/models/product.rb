3e3ab9326659f1d26b942915c7cfba98f962f9e8
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-27 18:45:00 -0300

    refactor: code format

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,30 +5,33 @@ class Product < ApplicationRecord
   # here the `allow_blank` is added so that it doens't return multiple errors, as it is required on the `presence` validation
   validates :image_url, allow_blank: true, format: {
     with: /.+\.((gif)|(jpe?g)|(png))\z/i,
-    message: 'must be a URL for GIF, JPG or PNG image.' }
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
   validate :image_has_to_exist
 
   private
-    def image_has_to_exist
-      return if image_url.blank?
-      errors.add(:image_url, "image doesn't exist: #{image_url}") unless asset_exists?(image_url)
-    end
 
-    def asset_exists?(path)
-      if Rails.env.production?
-        all_assets = Rails.application.assets_manifest.find_sources(path)
-        if all_assets
-          keys = Rails.application.assets_manifest.assets.keys
-          if keys.include?(path)
-            true
-          else
-          false
-          end
+  def image_has_to_exist
+    return if image_url.blank?
+
+    errors.add(:image_url, "image doesn't exist: #{image_url}") unless asset_exists?(image_url)
+  end
+
+  def asset_exists?(path)
+    if Rails.env.production?
+      all_assets = Rails.application.assets_manifest.find_sources(path)
+      if all_assets
+        keys = Rails.application.assets_manifest.assets.keys
+        if keys.include?(path)
+          true
         else
           false
         end
       else
-        Rails.application.assets.find_asset(path) != nil
+        false
       end
+    else
+      !Rails.application.assets.find_asset(path).nil?
     end
+  end
 end

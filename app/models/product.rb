fa87481c773dabe8931296c49a5652510e983ac5
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-27 18:46:14 -0300

    feat: add title minimum length check

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title, uniqueness: { case_sensitive: false }
   validates :title, :description, :image_url, presence: true
+  validates :title, length: { minimum: 10, message: 'product title is waaaay too short.' }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   # here the `allow_blank` is added so that it doens't return multiple errors, as it is required on the `presence` validation
   validates :image_url, allow_blank: true, format: {
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -60,4 +60,11 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
+
+  test 'product title must be at least 10 characters' do
+    product = new_product
+    product.title = '123456789'
+    assert product.invalid?
+    assert_equal ['product title is waaaay too short.'], product.errors[:title]
+  end
 end

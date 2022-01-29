77342fc29d674d2aed63621c3e8ed50f6f020511
Luiz Fernando Meier <lfmeier2@gmail.com>
2022-01-29 20:27:38 -0300

    feat: change the line item create test to check the items shown in the cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,10 +17,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create line_item' do
     assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: @line_item.product_id }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
 
-    assert_redirected_to Cart.find(a = session[:cart_id])
+    cart = Cart.find(a = session[:cart_id])
+    assert_redirected_to cart
+
+    follow_redirect!
+
+    assert_select 'h2', 'Your Pragmatic Cart'
+    assert_select 'li', 'Programming Ruby 1.9'
   end
 
   test 'should show line_item' do

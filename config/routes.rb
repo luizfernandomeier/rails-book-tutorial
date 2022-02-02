cc6741e79572d597c1f1c8764f8e21da02af515f
Luiz Fernando Meier <luiz.meier@trustedhealth.com>
2022-02-02 15:58:18 -0300

    refactor: ordering routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :orders
   root 'store#index', as: 'store_index' # this last part creates accessor methods so the tests continue to work
-  resources :products
   resources :carts
   resources :line_items
+  resources :orders
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

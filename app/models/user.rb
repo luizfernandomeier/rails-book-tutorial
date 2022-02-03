3282278465da4ce5e89a3930cf3c0d90e31ec801
Luiz Fernando Meier <luiz.meier@trustedhealth.com>
2022-02-03 16:16:37 -0300

    feat: add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_secure_password
+  validates :name, presence: true, uniqueness: true
+  has_secure_password # check if the password confirmation matches the password
 end

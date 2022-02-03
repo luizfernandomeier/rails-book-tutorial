5dcde0bba512eaef80ab19e69eb63eb22a16254d
Luiz Fernando Meier <luiz.meier@trustedhealth.com>
2022-02-03 12:50:47 -0300

    feat: add logic to send email on order created

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,14 @@
 class OrderMailer < ApplicationMailer
+  default from: 'Luiz F. <lfmeier@gmail.com>'
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.order_mailer.received.subject
   #
-  def received
-    @greeting = 'Hi'
-
-    mail to: 'to@example.org'
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation' # , content_type: 'text/plain'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -15,9 +16,8 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.shipped.subject
   #
-  def shipped
-    @greeting = 'Hi'
-
-    mail to: 'to@example.org'
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 end

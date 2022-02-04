66ab365a84c81277db8f36c0fa90579f0728f2de
Luiz Fernando Meier <luiz.meier@trustedhealth.com>
2022-02-04 10:41:04 -0300

    feat: add admin and login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'admin/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
   resources :users
   root 'store#index', as: 'store_index' # this last part creates accessor methods so the tests continue to work
   resources :carts

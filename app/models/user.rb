72de9b1677ca49532a32a8195adc4ef1c1bde00c
Luiz Fernando Meier <luiz.meier@trustedhealth.com>
2022-02-04 11:46:20 -0300

    feat: add validation to ensure the database contains at least one user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_secure_password # check if the password confirmation matches the password
+  after_destroy :ensure_an_admin_remains # transaction hook - we can create a stack of hooks
+
+  class Error < StandardError # this class exists so we can catch it explicitly on the controller
+  end
+
+  private
+
+  def ensure_an_admin_remains
+    # this will make the transaction fail
+    raise Error, "Can't delete last user" if User.count.zero?
+    # we could raise an ActiveRecord::Rollback exception if we didn't want the exception to be passed on
+  end
 end

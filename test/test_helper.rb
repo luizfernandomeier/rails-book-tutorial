68326d4f35e819bc32473e066e4c6b206d02fe44
Luiz Fernando Meier <luiz.meier@trustedhealth.com>
2022-02-04 11:12:21 -0300

    feat: add login logic to the tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative "../config/environment"
-require "rails/test_help"
+require_relative '../config/environment'
+require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
@@ -10,4 +10,34 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+# TODO: didn't get this very well.. setup is called automatically?
+module AuthenticationHelpers
+  def login_as(user)
+    if respond_to? :visit # TODO: what is this?
+      visit login_url
+      fill_in :name, with: user.name
+      fill_in :password, with: 'secret'
+      click_on 'Login'
+    else
+      post login_url, params: { name: user.name, password: 'secret' }
+    end
+  end
+
+  def logout
+    delete logout_url
+  end
+
+  def setup
+    login_as users(:one)
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include AuthenticationHelpers
+end
+
+class ActionDispatch::SystemTestCase
+  include AuthenticationHelpers
 end
